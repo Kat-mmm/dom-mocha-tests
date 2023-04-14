2ae32f73889f81a971a7b430517fbb854c28d4dc
Katlego Mothibe <Kaymothibe3@gmail.com>
2023-04-14 10:03:47 +0200

    calculate bill tests

---
+++ calculate-bill.js
@@ -0,0 +1,15 @@
+function totalPhoneBill(log){
+    var billString = log.toLowerCase().replace(/\s+/g, '').split(',');
+    
+    var bill = 0;
+    for(var i=0; i<billString.length; i++){
+      if(billString[i] === 'call'){
+        bill += 2.75;
+      }
+      else if(billString[i] === 'sms'){
+        bill += 0.65;
+      }
+    }
+    
+    return `R${bill.toFixed(2)}`;
+}
---
+++ test/calculate-bill.tests.js
@@ -0,0 +1,20 @@
+describe('Calculate Bill Tests', ()=>{
+    describe('Use string to calculate total phone bill', ()=>{
+        it('take a string of one call that is R2.75 and calculate the total', ()=>{
+            assert.equal('R2.75', totalPhoneBill('call'));
+        })
+        it('take a string of one sms that is R0.65 and calculate the total', ()=>{
+            assert.equal('R0.65', totalPhoneBill('sms'));
+        })
+        it("take a string of calls and sms's in uppercase and return the total bill", ()=>{
+            assert.equal('R6.15', totalPhoneBill('CALL, SMS, CALL'));
+        })
+        it("take a string of calls and sms's with no spaces in the string", ()=>{
+            assert.equal('R3.40', totalPhoneBill('sms,call'));
+        })
+        it("take a string of calls and sms's with spaces and  no spaces in the string, and a mix of uppercase and lowercase letters", ()=>{
+            assert.equal('R6.15', totalPhoneBill('sms,call, CALL'));
+        })
+    })
+
+})
